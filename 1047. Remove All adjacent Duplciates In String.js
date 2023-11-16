bf37911b147277f64ba0d8c1df5c690c53332ff8
Adefela Fakorode <adefakorode@gmail.com>
2023-11-15 23:34:54 -0500

    beats 98.33% of subs at time of sub

---
+++ 1047. Remove All adjacent Duplciates In String.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+ //ds: stack
+ 
+ var removeDuplicates = function(s) {
+    let stack = [];
+    for (const c of s) {
+        if (stack.length && stack[stack.length - 1] == c) {
+            stack.pop();
+        } else {
+            stack.push(c);
+        }
+    }
+    
+    return stack.join("");
+};
