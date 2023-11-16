08781715d3974eb818f667844d31b851624aa78f
Adefela Fakorode <adefakorode@gmail.com>
2023-11-16 00:58:50 -0500

    beats 65.85% of subs at time of sub

--- 844. Backspace String Compare.js
+++ 844. Backspace String Compare.js
@@ -3,22 +3,10 @@
  * @param {string} t
  * @return {boolean}
  */
-
-/*
-
-create 2 seprate stacks for s n t
-    if( stack == #) -> stack.pop()
-    else --> stack.push(char)
-
-    return stackS.join('') === stackT.join('');
-
-Time comp: O(n) , n being the size of the string of vat s and t
-
-*/
-
 var backspaceCompare = function(s, t) {
     let stackS = [];
     let stackT = [];
+
 
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
