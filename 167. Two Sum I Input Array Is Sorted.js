90a7ef990aa9d0b64042f27a9ded740bad804b8b
Adefela Fakorode <adefakorode@gmail.com>
2024-02-27 10:24:16 -0500

    Two Sum || hashmap solution

--- 167. Two Sum I Input Array Is Sorted.js
+++ 167. Two Sum I Input Array Is Sorted.js
@@ -16,7 +16,7 @@ ds: two ptrs
 
  */
 
-     var twoSum = function (numbers, target) {
+/*     var twoSum = function (numbers, target) {
         let left = 0
         let right = numbers.length - 1
     
@@ -32,4 +32,42 @@ ds: two ptrs
             }
     
         }
-    };
+    };*/
+
+    /**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+/*
+ds: hashmap solution
+    init map
+    key: num
+    value: index
+
+    init index = 1
+    get complement = target - num
+    if map has comp
+        return comp and its index
+
+    else{
+        map add num n its index
+        index++
+    }
+
+ */
+
+var twoSum = function (numbers, target) {
+    let map = new Map()
+    let index = 1
+    for(let num of numbers){
+        let complement = target - num
+        if(map.has(complement)){
+            return [map.get(complement), index]
+        }
+        map.set(num, index)
+        index++
+    }
+};
